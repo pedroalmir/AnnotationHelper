9f969a6628d84652543d30579db98971341929df
Ankit Gupta <ankit.gupta2801@gmail.com>
2013-04-11 11:39:32 -0700

    added initial version of annotated lookup class

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 AnnotationHelper
 ================
 
-A simple library to dynamically load classes from jars and get classes using annotation.
+A simple library to load jar and get classes with annotation. This is particularly useful when creating a simple applications that want to support plugin-based extensibility using Java Annotations.
---
+++ core/src/main/java/annotation/AnnotatedClassLookup.java
@@ -0,0 +1,115 @@
+package annotation;
+import java.io.File;
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.apache.commons.collections15.MultiMap;
+import org.apache.commons.collections15.multimap.MultiHashMap;
+
+/**
+ * A helper class to load jars in a directory and get the classes based on
+ * annotations.
+ * 
+ * @author Ankit Gupta - ankit.gupta2801@gmail.com
+ * 
+ */
+public class AnnotatedClassLookup {
+
+	private MultiMap<Annotation, Class<?>> pluginMap;
+
+	private File classDirectory;
+
+	public AnnotatedClassLookup(File directory) {
+		this.classDirectory = directory;
+		loadPlugins();
+	}
+
+	/**
+	 * Load plugins from the plugin directory.
+	 * 
+	 */
+	private void loadPlugins() {
+		if (classDirectory.isDirectory()) {
+
+			// get the list of files contained in the plugins directory.
+			File[] containedFiles = classDirectory.listFiles();
+
+			for (File containedFile : containedFiles) {
+				// if the file is a Jar, look up the jar for implementations.
+				if (!containedFile.isDirectory() && containedFile.getAbsolutePath().endsWith(
+						".jar")) {
+					pluginMap = getPluginMap(containedFile);
+				}
+			}
+		} else {
+			System.err.println("Plugin Directory does not exist");
+		}
+	}
+
+	/**
+	 * Gets a mapping from annotation type to the corresponding classes.
+	 * 
+	 * @param file the file to look for getting the mapping
+	 * @return a multimap containing mappings from an annotation to the
+	 *         corresponding classes.
+	 */
+	private MultiMap<Annotation, Class<?>> getPluginMap(File file) {
+		MultiMap<Annotation, Class<?>> pluginMap =
+				new MultiHashMap<Annotation, Class<?>>();
+
+		try {
+			URLClassLoader jarLoader = loadJar(file.getAbsoluteFile());
+
+			JarFile jarFile = new JarFile(file.getAbsoluteFile());
+			Enumeration<JarEntry> entries = jarFile.entries();
+			while (entries.hasMoreElements()) {
+				JarEntry jarEntry = entries.nextElement();
+				String className = jarEntry.toString().replace("/", ".");
+				if (className.endsWith(".class")) {
+					className = className.replace(".class", "");
+					try {
+						Class<?> loadedClass = jarLoader.loadClass(className);
+						Annotation[] annotations = loadedClass.getAnnotations();
+						for(Annotation annotation: annotations){
+							pluginMap.put(annotation, loadedClass);
+						}
+						
+					} catch (ClassNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch (MalformedURLException e) {
+			System.err.println(file + " plugin jar could not be loaded");
+		} catch (IOException e) {
+			System.err.println("Loading jar file " + file + " failed.");
+		}
+
+		return pluginMap;
+	}
+
+
+	/**
+	 * Loads the jar and return the class loader used for loading.
+	 * @param jarToBeLoaded the jar file to be loaded.
+	 * @return the {@link URLClassLoader} used to load the jar.
+	 * @throws IOException if the jar file could not be read.
+	 */
+	private URLClassLoader loadJar(File jarToBeLoaded) throws IOException {
+		URL jarfileURL =
+				new URL("jar:file://" + jarToBeLoaded.getAbsolutePath() + "!/");
+		URLClassLoader urlClassLoader =
+				new URLClassLoader(new URL[] { jarfileURL });
+		return urlClassLoader;
+	}
+
+	public MultiMap<Annotation, Class<?>> getPluginMap() {
+		return pluginMap;
+	}
+}
